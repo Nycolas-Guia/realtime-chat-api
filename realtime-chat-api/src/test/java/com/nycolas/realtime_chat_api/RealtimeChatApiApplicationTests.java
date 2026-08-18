aa94e533ecae9f42bca5e73c9dad26cee06a3672
Nycolas <nycolas.guia@hotmail.com>
2026-08-17 22:02:15 -0300

    setup inicial do spring boot e docker-compose

---
+++ realtime-chat-api/src/test/java/com/nycolas/realtime_chat_api/RealtimeChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nycolas.realtime_chat_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeChatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
