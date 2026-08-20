896c4db1f4b0d633b5af0701b8003fa882ead6be
Nycolas <nycolas.guia@hotmail.com>
2026-08-20 20:35:19 -0300

    feat(api): adiciona broadcast via websocket na criacao de novas salas

--- src/main/java/com/nycolas/realtime_chat_api/service/ChatRoomService.java
+++ src/main/java/com/nycolas/realtime_chat_api/service/ChatRoomService.java
@@ -1,7 +1,9 @@
 package com.nycolas.realtime_chat_api.service;
 
 import com.nycolas.realtime_chat_api.domain.ChatRoom;
+import com.nycolas.realtime_chat_api.dto.ChatRoomResponseDTO;
 import com.nycolas.realtime_chat_api.repository.ChatRoomRepository;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,15 +13,25 @@ public class ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
 
-    public ChatRoomService(ChatRoomRepository chatRoomRepository) {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public ChatRoomService(ChatRoomRepository chatRoomRepository, SimpMessagingTemplate messagingTemplate) {
         this.chatRoomRepository = chatRoomRepository;
+        this.messagingTemplate = messagingTemplate;
     }
 
+    /**
+     * Cria uma nova sala no banco de dados e notifica todos os usuários conectados.
+     */
     public ChatRoom createRoom(String name) {
         ChatRoom newRoom = new ChatRoom();
         newRoom.setName(name);
 
-        return chatRoomRepository.save(newRoom);
+        ChatRoom savedRoom = chatRoomRepository.save(newRoom);
+        ChatRoomResponseDTO responseDTO = new ChatRoomResponseDTO(savedRoom);
+        messagingTemplate.convertAndSend("/topic/rooms", responseDTO);
+
+        return savedRoom;
     }
 
     public List<ChatRoom> getAllRooms() {
