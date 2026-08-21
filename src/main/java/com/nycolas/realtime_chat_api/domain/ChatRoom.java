e9efdc7cb6d5ae448859b2d242adcaa0deea18a0
Nycolas <nycolas.guia@hotmail.com>
2026-08-20 21:06:07 -0300

    feat(domain): adiciona relacao many-to-many entre usuarios e salas para suporte a chats privados

--- src/main/java/com/nycolas/realtime_chat_api/domain/ChatRoom.java
+++ src/main/java/com/nycolas/realtime_chat_api/domain/ChatRoom.java
@@ -2,6 +2,8 @@ package com.nycolas.realtime_chat_api.domain;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -15,7 +17,20 @@ public class ChatRoom {
     @Column(nullable = false)
     private String name;
 
+    // Define se é um chat privado 1x1 (true) ou um grupo (false)
+    @Column(nullable = false)
+    private boolean isDirectMessage = false;
+
     private LocalDateTime createdAt = LocalDateTime.now();
+
+    // Cria a tabela intermediária que liga os Usuários às Salas
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "tb_chat_room_members",
+            joinColumns = @JoinColumn(name = "room_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> members = new HashSet<>();
 
     // ==========================================
     // GETTERS E SETTERS
@@ -26,6 +41,12 @@ public class ChatRoom {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
+    public boolean isDirectMessage() { return isDirectMessage; }
+    public void setDirectMessage(boolean directMessage) { isDirectMessage = directMessage; }
+
     public LocalDateTime getCreatedAt() { return createdAt; }
     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
+    public Set<User> getMembers() { return members; }
+    public void setMembers(Set<User> members) { this.members = members; }
 }
